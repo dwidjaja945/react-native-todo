593ec357a29f4f2e2a633190ad4d5f5a54bb4c04
Dylan Widjaja <dwidjaja945@gmail.com>
2018-07-31 17:56:53 -0700

    initial commit

---
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/StaticAnalyzer/React/React/normal/x86_64/PrivateDataBase.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/dylanwidjaja/Desktop/projects/react-native-todo/node_modules/react-native/ReactCommon/privatedata/PrivateDataBase.cpp \
+  /Users/dylanwidjaja/Desktop/projects/react-native-todo/node_modules/react-native/ReactCommon/privatedata/PrivateDataBase.h

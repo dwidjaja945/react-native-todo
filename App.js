8a2e3a1a7350578706791018f29e04ad27478d74
Dylan Widjaja <dwidjaja945@gmail.com>
2018-07-31 22:31:36 -0700

    can now display dummy list data onto app styling will need work, but fortunately similar enough to css everything in objects rather than css. reference documentation for different terminology Next will be to connect to mongoDB

--- App.js
+++ App.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import styles from './styles/app_style';
+
+import TodoList from './components/TodoList';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Todo List</Text>
-        <View>
-          <Text> Does this work? </Text>
-        </View>
-      </View>
-    );
-  }
-}
+	render() {
+		return (
+			<View style={styles.container}>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    marginTop: 40
-  },
-  text : {
-    fontSize: 30
-  }
-});
+				<View className="header" >
+					<Text style={styles.header}>Todo List</Text>
+				</View>
+
+				<TodoList />
+
+			</View>
+		);
+	}
+}
